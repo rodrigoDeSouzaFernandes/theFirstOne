47ac418a6afca23d01a82548e44084746a94b664
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-10-23 17:23:58 -0300

    exercicios 1 e 2

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -0,0 +1,22 @@
+//1
+let a = 10
+let b = 5
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+
+//2
+if(a > b){
+    return a
+}
+else if(a < b){
+    return b
+}
+else{
+    console.log("Os números são iguais")
+}
+
+
