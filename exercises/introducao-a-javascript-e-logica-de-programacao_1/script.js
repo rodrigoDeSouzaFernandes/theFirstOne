e85f424a81ecf5ebbb154fd5c1b09d8bb1f5228b
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-10-23 17:45:11 -0300

    exercicio 5

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,51 +1,61 @@
 //1
-let a = 10
-let b = 5
+let a = 10;
+let b = 5;
 
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a / b)
-console.log(a % b)
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
 //2
 if(a > b){
-    console.log (a)
+    console.log (a);
 }
 else if(a < b){
-    console.log (b)
+    console.log (b);
 }
 else{
-    console.log("Os números são iguais")
+    console.log("Os números são iguais");
 }
 
 //3
-let c = 3
-let d = 4
-let e = 6
+let c = 3;
+let d = 4;
+let e = 6;
 
 if(c > d && c > e){
-    console.log (c)
+    console.log (c);
 }
 else if(d > c && d > e){
-    console.log (d)
+    console.log (d);
 }
 else if(e > d && e > c){
-    console.log (e)
+    console.log (e);
 }
 else{
-    console.log("Os numeros são iguais")
+    console.log("Os numeros são iguais");
 }
 
 //4
 if(a > 0){
-    console.log(true)
+    console.log(true);
 }
 else if(a < 0){
-    console.log(false)
+    console.log(false);
 }
 else{
-    console.log(0)
+    console.log(0);
 }
 
 //5
+if(a + b + c == 180){
+    console.log(true);
+    return true;
+}
+else {
+    console.log('Ângulos inválidos');
+    return false;
+}
+
+//6
