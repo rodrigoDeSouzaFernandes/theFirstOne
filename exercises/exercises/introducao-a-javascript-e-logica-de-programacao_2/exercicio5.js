03ae41804815f7ce9ca26fca9dbfb90de9a5f5de
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-10-26 18:26:27 -0300

    exercicio 7 concluido

--- exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5.js
+++ exercises/exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio5.js
@@ -1,12 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let biggerNumber = 0
-let anothers = []
+
 for(let index = 0; index < numbers.length; index++){
   if(numbers[index] > biggerNumber){
     biggerNumber = numbers[index]
   }
   else{
-    anothers.push(numbers[index]);
+    
   }
-  
+}
+
   console.log(biggerNumber);
