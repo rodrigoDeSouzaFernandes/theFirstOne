e724a0b840581e86c0d5c302794304e819a21d15
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-10-27 15:51:17 -0300

    quadrado de asteriscos

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/02trianguloretangulo.js
@@ -0,0 +1,8 @@
+let n = 7
+let quadradoAsteriscos = ""
+for(let line = 1; line <= n; line += 1){
+    for(let col = n; col == n; col += 1){
+        quadradoAsteriscos += "*";
+    }
+    console.log(quadradoAsteriscos)
+}
