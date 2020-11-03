0103b6b1d70f1137310c466be035e84ee7a011b4
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-11-03 16:22:43 -0300

    exercicio 2

--- exercises/javascript-dom-eventos-e-web-storage_2/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/script.js
@@ -4,13 +4,22 @@ Exercício 5.2 - JavaScript DOM como filho da tag body;
 */
 let titleText = "Exercício 5.2 - JavaScript DOM ";
 
-let title = document.querySelector('body');
+let body = document.querySelector('body');
 
 let text = document.createElement('h1');
 text.innerText = titleText;
 
-title.appendChild(text);
+body.appendChild(text);
 
 /*
 2 -Adicione a tag div com a classe main-content como filho da tag body;
+*/
+let mainContent = document.createElement('div');
+mainContent.getElementsByClassName = "main-content";
+
+body.appendChild(mainContent);
+
+/*
+3 -Adicione a tag div com a classe center-content 
+como filho da tag div criada no passo 2;
 */
