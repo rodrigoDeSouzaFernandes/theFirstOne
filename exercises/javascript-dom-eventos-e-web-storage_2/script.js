de8e1cb577c7ab5a9432ac3506bbcf75e6bc4887
Rodrigo de Souza Fernandes <rdesouza2008@gmail.com>
2020-11-03 18:56:43 -0300

    até o 9 concluido

--- exercises/javascript-dom-eventos-e-web-storage_2/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/script.js
@@ -15,7 +15,7 @@ body.appendChild(text);
 2 - Adicione a tag div com a classe main-content como filho da tag body;
 */
 let mainContent = document.createElement('div');
-mainContent.getElementsByClassName = "main-content";
+mainContent.className = "main-content";
 
 body.appendChild(mainContent);
 
@@ -24,7 +24,7 @@ body.appendChild(mainContent);
 como filho da tag div criada no passo 2;
 */
 let centerContent = document.createElement('div');
-centerContent.getElementsByClassName = "center-content";
+centerContent.className = "center-content";
 
 mainContent.appendChild(centerContent);
 
@@ -36,3 +36,59 @@ let firstParagraph = document.createElement('p');
 firstParagraph.innerText = "Vivendo e aprendendo";
 
 centerContent.appendChild(firstParagraph);
+
+/* 
+5 - Adicione a tag div com a classe left-content 
+como filho da tag div criada no passo 2;
+*/
+let leftContent = document.createElement("div");
+leftContent.className = "left-content";
+
+mainContent.appendChild(leftContent);
+
+/*
+6 - Adicione a tag div com a classe right-content como filho
+ da tag div criada no passo 2;
+*/
+let rightContent = document.createElement('div');
+rightContent.className = "right-content";
+
+mainContent.appendChild(rightContent);
+
+/*
+7 - Adicione uma imagem com src configurado para o valor 
+https://picsum.photos/200 e classe small-image. Esse elemento 
+deve ser filho do div criado no passo 5;
+*/
+let image = document.createElement("img");
+image.className = 'small-image';
+image.src = "https://picsum.photos/200";
+
+leftContent.appendChild(image);
+
+/*
+8 - Adicione uma lista não ordenada com os valores de 1 a 10 por extenso 
+como valores da lista. Essa lista deve ser filha do div criado no passo 6;
+ */
+let lista = document.createElement('ul');
+
+rightContent.appendChild(lista);
+
+let numbers = ["um",'dois','três','quatro','cinco','seis','sete','oito','nove','dez']
+
+
+
+for(let index = 0; index < numbers.length; index +=1){
+    let num = numbers[index];
+    let numList = document.createElement('li');
+    numList.innerText = num;
+
+    lista.appendChild(numList);
+}
+
+// 9 - Adicione 3 tags h3, todas sendo filhas do div criado no passo 2.
+
+for(index = 0; index < 3; index +=1){
+    let addh = document.createElement('h3');
+   mainContent.appendChild(addh) ;
+}
