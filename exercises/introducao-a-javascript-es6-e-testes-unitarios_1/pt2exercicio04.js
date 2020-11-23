0375cac3779619ade14ebdedd9238e8609ed555d
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-11-23 19:08:39 -0300

    continuação exercicio 4

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/pt2exercicio04.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/pt2exercicio04.js
@@ -1,3 +1,19 @@
 const findX = (string, subistitute) => string.replace('x', subistitute);
 
 console.log(findX('trybe x aqui', 'bebeto'));
+
+//  function 2
+
+let skills = ['javascript', 'css', 'html', 'empatia' , 'auto conhecimento'];
+
+const function2 = (function1, skill) => {
+   return `${function1}
+    minhas cinco principais habilidades são: 
+    ${skill.sort()[0]}
+    ${skill.sort()[1]}
+    ${skill.sort()[2]}
+    ${skill.sort()[3]}
+    ${skill.sort()[4]}`
+}
+
+console.log(function2(findX('trybe x aqui', 'bebeto'), skills));
