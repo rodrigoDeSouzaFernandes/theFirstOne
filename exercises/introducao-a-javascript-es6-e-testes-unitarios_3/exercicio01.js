0ca87e990b838cc40660bf451562cdb9c3b60edc
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-11-25 16:00:03 -0300

    1 ao 3

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio01.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+
+assert.equal(sum(4,5), 9);
+assert.equal(sum(4,'5'), 9); // <-Error 'parameters must be numbers'
