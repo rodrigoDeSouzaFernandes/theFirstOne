59f62df6a7190981510b1be642564ac8a20bbabf
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-11-25 16:14:53 -0300

    exercicio 05

---
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_3/exercicio05.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+const obj1 = {
+  title: 'My Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  description: 'My Description',
+  title: 'My Title',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+const obj1Copia = {
+    title: 'My Title',
+    description: 'My Description',
+  };
+
+assert.deepStrictEqual(obj1, obj1Copia)
