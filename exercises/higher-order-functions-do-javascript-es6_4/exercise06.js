c3c66608a482f4e3b5c009add4be251ab09ab38b
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-12-03 15:43:18 -0300

    refatorando exercicio 6

--- exercises/higher-order-functions-do-javascript-es6_4/exercise06.js
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise06.js
@@ -4,14 +4,7 @@ const palio = ["Palio", "Fiat", 2019]
 const shelbyCobra = ["Shelby Cobra", "Ford", 1963]
 const chiron = ["Chiron", "Bugatti", 2016]
 
-const toObject = ([a, b, c]) => {
-  let obj = {
-    name: a,
-    brand: b,
-    year: c
-  };
-  return obj
-}
+const toObject = ([name, brand, year]) => ({name, brand, year};
 
 assert.deepStrictEqual(toObject(palio), { name: "Palio", brand: "Fiat", year: 2019 })
 assert.deepStrictEqual(toObject(shelbyCobra), { name: "Shelby Cobra", brand: "Ford", year: 1963 })
