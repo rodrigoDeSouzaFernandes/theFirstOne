e37aeaf5ab51e4c901598c388acc0f28ad3e9131
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-12-03 15:27:33 -0300

    exercicio 4

---
+++ exercises/higher-order-functions-do-javascript-es6_4/exercise04.js
@@ -0,0 +1,38 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+const bornIn20Century = (born) => born > 1900 && born < 2001
+const isAustralian = (nationality) => nationality === 'Australian'
+const filterPeople = (array) => array.filter(({nationality, bornIn}) => bornIn20Century(bornIn) && isAustralian(nationality))
+
+const filteredPeople = filterPeople(people)
+
+assert.deepStrictEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepStrictEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
