1cd27800b0b494308edcdbc64a780955c1298e42
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-12-09 19:34:46 -0300

    exercicio 5

--- exercises/javascript-assincrono-e-promises_2/exercicio02.js
+++ exercises/javascript-assincrono-e-promises_2/exercicio02.js
@@ -1,20 +1,21 @@
 const arrayPromise = () => {
   return new Promise((resolved, reject) => {
-    const arrayDivisor = [2, 3, 5, 10]
+    
     let randomArray = [];
     let sum = 0;
       for (let i = 1; i <= 10; i += 1) {
-        randomArray.push(Math.floor(Math.random() * 50)**2);
+        randomArray.push(Math.floor(Math.random() * 50 + 1)**2);
       }
     sum = randomArray.reduce((acc, cur) => acc + cur);
     if (sum < 8000) {
-      resolved(arrayDivisor.map((elem) => Math.round(sum/elem)))
+      resolved(sum)
     } else {
       reject ('É mais de oito mil! Essa promise deve estar quebrada!');
     }
     });
 };
-
+const arrayDivisor = [2, 3, 5, 10]
 arrayPromise()
-.then(elem => console.log(elem))
-.catch(error => console.log(error));
+.then(elem => (arrayDivisor.map((a) => Math.round(elem/a))))
+.then(array => array.reduce((a, b) => a+b))
+.catch(error => console.log(error))
