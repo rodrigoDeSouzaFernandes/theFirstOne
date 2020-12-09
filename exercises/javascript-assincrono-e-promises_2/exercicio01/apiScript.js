4ae182480ea63c211f401b31f04b63661b63fc3c
Rodrigo de Souza <rdesouza2008@gmail.com>
2020-12-09 18:40:46 -0300

    site gerador de piadas

---
+++ exercises/javascript-assincrono-e-promises_2/exercicio01/apiScript.js
@@ -0,0 +1,19 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+  
+  fetch(API_URL, myObject)
+    .then(response => response.json())
+    .then(({joke}) => appendJoke(joke))
+};
+
+const appendJoke = (joke) => {
+  const jokeContainer = document.querySelector('#jokeContainer');
+  jokeContainer.innerHTML = joke
+}
+
+window.onload = () => fetchJoke();
