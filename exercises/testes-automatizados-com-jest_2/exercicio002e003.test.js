22fc8b8d5df44b3685bc684fe5d9fc7706b4c5ed
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-05 18:44:23 -0300

    exercicio 3 completo

--- exercises/testes-automatizados-com-jest_2/exercicio002e003.test.js
+++ exercises/testes-automatizados-com-jest_2/exercicio002e003.test.js
@@ -1,10 +1,10 @@
-const { getUserName} = require('./exercicio002e003');
+const { getUserName } = require('./exercicio002e003');
 
 // describe('para o caso em que o usuário é encontrado', () => {
 //     it('encontrar paul', () => {
 //         expect.assertions(1);
 //         return getUserName(5).then((data) => {
-//         expect(data).toEqual('Paul');
+//         expect(data).toBe('Paul');
 //         });
 //     });
 // });
@@ -13,7 +13,27 @@ describe('para o caso em que o usuário é encontrado', () => {
     it('encontrar paul', async () => {
         const data = await getUserName(5);
         expect.assertions(1);
-        expect(data).toEqual('Paul');
-        
+        expect(data).toBe('Paul');
+
+    });
+});
+
+/*describe('para o caso em que o usuário não é encontrado', () => {
+    it('should not find', () => {
+        expect.assertions(1)
+        return getUserName(7).catch(data => {
+            expect(data).toEqual({ error: 'User with 7 not found.' })
+        });
+    });
+});*/
+
+describe('para o caso em que o usuário não é encontrado', () => {
+    it('nao deve encontrar', async () => {
+        try {
+            const data = await getUserName(7);
+        } catch (error) {
+            expect.assertions(1)
+            expect(error).toEqual({ error: 'User with 7 not found.' })
+        }
     });
 });
