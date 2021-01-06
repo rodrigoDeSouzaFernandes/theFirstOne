210c48221249a115c69f0b583f24fe480241b559
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-01-06 15:41:48 -0300

    exercise01

--- exercises/testes-automatizados-com-jest_3/exercise1.test.js
+++ exercises/testes-automatizados-com-jest_3/exercise1.test.js
@@ -1,1 +1,14 @@
-const randomNumber = require('./exercise01');
+let randomNumber = require('./exercise01');
+
+describe('Testing random number', () => {
+  it('should return 10', () => {
+    randomNumber = jest
+    .fn()
+    .mockReturnValue(10 );
+
+    randomNumber();
+    expect(randomNumber).toHaveBeenCalled();
+    expect(randomNumber()).toBe(10);
+    expect(randomNumber).toHaveBeenCalledTimes(2);
+  })
+})
