327a3f127f680353c526c2136c347e10174de90a
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-05-06 17:59:08 -0300

    1-4

---
+++ exercises/exercise02.js
@@ -0,0 +1,6 @@
+db.movies.find({
+	category: {
+		$all: [ 'action' ]
+	},
+	imdbRating: { $gt: 7 }
+});
---
+++ exercises/exercise03.js
@@ -0,0 +1,12 @@
+db.movies.updateOne(
+	{
+		title: 'Batman'
+	},
+	{
+		$push: {
+			ratings: {
+				$each: [ 85, 100, 102, 105 ]
+			}
+		}
+	}
+);
