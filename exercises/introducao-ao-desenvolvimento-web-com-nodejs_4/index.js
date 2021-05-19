657221695092e32b72ec4a40be3e8746745a1f88
Rodrigo de Souza <rdesouza2008@gmail.com>
2021-05-19 19:28:14 -0300

    exercicio 3

--- exercises/introducao-ao-desenvolvimento-web-com-nodejs_4/index.js
+++ exercises/introducao-ao-desenvolvimento-web-com-nodejs_4/index.js
@@ -5,8 +5,24 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// exercicio 1
 app.get("/ping", (_req, res) => {
   res.json({message: 'pong'})
+});
+
+// exercicio 2
+app.post("/hello", (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}!` })
+})
+
+// exercicio 3
+app.post("/greetings", (req, res) => {
+  const {name, age} = req.body;
+  if(Number(age) <= 17) {
+    res.status(401).json({ message: `Unauthorized` })
+  }
+  res.status(200).json({ message: `Hello: ${name}` });
 });
 
 app.use((err, req, res, next) => {
